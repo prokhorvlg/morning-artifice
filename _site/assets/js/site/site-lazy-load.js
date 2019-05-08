92c119d49f7d68be7ccf9e0c597a96da7c40ff47
Valentin Sigalov <vlgstudent036@gmail.com>
2019-05-08 10:58:35 -0400

    lazy load

---
+++ _site/assets/js/site/site-lazy-load.js
@@ -0,0 +1,24 @@
+const config = {
+  rootMargin: '0px 0px 50px 0px',
+  threshold: 0
+};
+
+let observer = new IntersectionObserver(function(entries, self) {
+  entries.forEach(entry => {
+    if(entry.isIntersecting) {
+      preloadImage(entry.target);
+      self.unobserve(entry.target);
+    }
+  });
+}, config);
+
+const imgs = document.querySelectorAll('[data-src]');
+imgs.forEach(img => {
+  observer.observe(img);
+});
+
+function preloadImage(img) {
+  const src = img.getAttribute('data-src');
+  if (!src) { return; }
+  img.src = src;
+}
