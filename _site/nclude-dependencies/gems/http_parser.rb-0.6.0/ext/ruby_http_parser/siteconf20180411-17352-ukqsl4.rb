9086e742aacf69469f189d1a4c2748302c606f81
Valentin Sigalov <vlgstudent036@gmail.com>
2019-09-09 11:19:06 -0400

    Removed generated site

--- _site/nclude-dependencies/gems/http_parser.rb-0.6.0/ext/ruby_http_parser/siteconf20180411-17352-ukqsl4.rb
+++
@@ -1,6 +0,0 @@
-require 'rbconfig'
-dest_path = "./.gem.20180411-17352-6ar426"
-RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
-RbConfig::CONFIG['sitearchdir'] = dest_path
-RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
-RbConfig::CONFIG['sitelibdir'] = dest_path
